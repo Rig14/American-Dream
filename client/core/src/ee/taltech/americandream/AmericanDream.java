b8aea79a1267d476f56e6f4fea746021cf3d7a60
ririvi <ririvi@taltech.ee>
2024-02-13 17:36:25 +0200

    Added Server-Client connection | When server is started 'Test' is sent to the server from client

--- client/core/src/ee/taltech/americandream/AmericanDream.java
+++ client/core/src/ee/taltech/americandream/AmericanDream.java
@@ -4,28 +4,48 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.esotericsoftware.kryonet.Client;
+
+import java.io.IOException;
 
 public class AmericanDream extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    Texture img;
+    private Client client;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+        client = new Client();
+        client.start();
+        try {
+            client.connect(5000, "localhost", 8080, 8081);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        client.sendTCP("Start");
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(1, 0, 0, 1);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+        client.sendTCP("test");
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+        client.close();
+        try {
+            client.dispose();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
