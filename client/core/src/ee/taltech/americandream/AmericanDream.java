172f6afcd12bdf2ca3daf892f1065d9efc45f5c7
ririvi <ririvi@taltech.ee>
2024-02-13 18:00:26 +0200

    Picture on the screen is moving when A or D keys are pressed

--- client/core/src/ee/taltech/americandream/AmericanDream.java
+++ client/core/src/ee/taltech/americandream/AmericanDream.java
@@ -2,6 +2,7 @@ package ee.taltech.americandream;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -9,12 +10,14 @@ import com.esotericsoftware.kryonet.Client;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class AmericanDream extends ApplicationAdapter {
     SpriteBatch batch;
     Texture img;
     private Client client;
+    private Point player = new Point(0, 0);
 
     @Override
     public void create() {
@@ -32,7 +35,11 @@ public class AmericanDream extends ApplicationAdapter {
             @Override
             public void received(Connection connection, Object object) {
                 super.received(connection, object);
-                Gdx.app.log("From Server: ", object.toString());
+                if (!(object instanceof String)) return;
+                String response = (String) object;
+                Gdx.app.log("Received", response);
+                player = new Point(Integer.parseInt(response.split(",")[0]), Integer.parseInt(response.split(",")[1]));
+
             }
         });
     }
@@ -41,9 +48,17 @@ public class AmericanDream extends ApplicationAdapter {
     public void render() {
         ScreenUtils.clear(1, 0, 0, 1);
         batch.begin();
-        batch.draw(img, 0, 0);
+        batch.draw(img, player.x, player.y);
         batch.end();
-        client.sendTCP("test");
+
+        // left arrow key
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            client.sendTCP("Left");
+        }
+        // right arrow key
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            client.sendTCP("Right");
+        }
     }
 
     @Override
