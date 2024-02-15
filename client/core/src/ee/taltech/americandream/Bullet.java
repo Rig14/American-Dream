c0d4ec2235a3daa826f1a383fc9ddb974cfe8f6a
eliaspriinits <eliaspriinits@gmail.com>
2024-02-15 16:40:39 +0200

    bullet variables

---
+++ client/core/src/ee/taltech/americandream/Bullet.java
@@ -0,0 +1,23 @@
+package ee.taltech.americandream;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Vector;
+
+public class Bullet {
+    public int speedBullet = 500;
+    public Sprite bulletSprite;
+    public Vector2 position;
+    public void update() {
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+
+        }
+    }
+    public void draw(SpriteBatch Sprite) {
+
+    }
+}
