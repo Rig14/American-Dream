614ae9c6569b13f1b29aa897d921b53a48b7b3be
ririvi <ririvi@taltech.ee>
2024-02-17 11:47:04 +0200

    Server side logic

---
+++ server/src/main/java/ee/taltech/americandream/server/Game.java
@@ -0,0 +1,45 @@
+package ee.taltech.americandream.server;
+
+import com.esotericsoftware.kryonet.Connection;
+import helper.PlayerState;
+import helper.packet.GameStateMessage;
+
+import static helper.Constants.TICK_RATE;
+
+public class Game extends Thread {
+    private boolean running = true;
+    private Player[] players;
+    public Game(Connection[] connections) {
+        players = new Player[connections.length];
+        // start game with connections
+        // make players from connections
+        for (int i = 0; i < connections.length; i++) {
+            players[i] = new Player(connections[i], this, i+1);
+        }
+    }
+
+    public void run() {
+        while (running) {
+            try {
+                // construct game state message
+                GameStateMessage gameStateMessage = new GameStateMessage();
+                gameStateMessage.playerStates = new PlayerState[players.length];
+                for (int i = 0; i < players.length; i++) {
+                    gameStateMessage.playerStates[i] = players[i].getState();
+                }
+                // send game state message to all players
+                for (Player player : players) {
+                    player.sendGameState(gameStateMessage);
+                }
+                // message is sent every game tick
+                Thread.sleep(1000 / TICK_RATE);
+            } catch (InterruptedException e) {
+                running = false;
+            }
+        }
+    }
+
+    public void end() {
+        running = false;
+    }
+}
--- server/src/main/java/ee/taltech/americandream/server/GameServer.java
+++ server/src/main/java/ee/taltech/americandream/server/GameServer.java
@@ -1,40 +1,65 @@
 package ee.taltech.americandream.server;
 
+import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
 
 import java.awt.*;
 import java.io.IOException;
+import java.util.Arrays;
+
+import static helper.Constants.LOBBY_SIZE;
+import static helper.Constants.PORTS;
 
 public class GameServer {
-    private Server server = new Server();
-
-    private Point player = new Point(0, 0);
+    private Server server;
 
     public GameServer() {
+        // setup server and open ports
         this.server = new Server();
         try {
             server.start();
-            server.bind(8080, 8081);
+            server.bind(PORTS[0], PORTS[1]);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        server.addListener(new Listener(){
-            public void received(Connection connection, Object object) {
-                if (!(object instanceof String request)) return;
-                if (request.equals("Left")) {
-                    player.x -= 1;
+        // register used classes
+        registerClasses();
+
+        // add listener for new connections
+        server.addListener(new Listener() {
+            Connection[] connections = new Connection[LOBBY_SIZE];
+
+            @Override
+            public void connected(Connection connection) {
+                super.connected(connection);
+                // loop through connections and add new connection to first empty slot
+                for (int i = 0; i < connections.length; i++) {
+                    // if connection is empty or not connected, add new connection
+                    if (connections[i] == null || !connections[i].isConnected()) {
+                        connections[i] = connection;
+                        break;
+                    }
                 }
-                if (request.equals("Right")) {
-                    player.x += 1;
+                if (Arrays.stream(connections).allMatch(c -> c != null && c.isConnected())) {
+                    // if all connections are filled, start game with the connections
+                    Game game = new Game(connections);
+                    game.start();
+                    // clear connections if all are connected and game is started
+                    connections = new Connection[LOBBY_SIZE];
                 }
-                connection.sendTCP(player.x + "," + player.y);
             }
         });
     }
 
     public static void main(String[] args) {
-        GameServer gameServer = new GameServer();
+        new GameServer();
+    }
+
+    private void registerClasses() {
+        // register classes for serialization
+        Kryo kryo = server.getKryo();
+
     }
 }
