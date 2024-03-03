0f009f0e99cae9a869d14d00f9781875571a824c
rasand <rasand@taltech.ee>
2024-03-03 14:54:36 +0200

    Added trump and biden sprites

--- client/core/src/helper/Textures.java
+++ client/core/src/helper/Textures.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.graphics.Texture;
 public class Textures {
 
     public final static Texture BULLET_TEXTURE = new Texture("bullet2-transformed.png");
-    public final static Texture playerTexture = new Texture("badlogic.jpg");
+    public final static Texture PLAYER_TEXTURE = new Texture("badlogic.jpg");
+    public final static Texture TRUMP_TEXTURE = new Texture("trump.jpg");
+    public final static Texture BIDEN_TEXTURE = new Texture("biden.jpg");
 
 }
