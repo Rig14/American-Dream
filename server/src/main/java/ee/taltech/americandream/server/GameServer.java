101c0647be8c69286e212ca559529a8d2dd951ea
ririvi <ririvi@taltech.ee>
2024-03-14 14:50:24 +0200

    added lobby class in backend

--- server/src/main/java/ee/taltech/americandream/server/GameServer.java
+++ server/src/main/java/ee/taltech/americandream/server/GameServer.java
@@ -14,16 +14,26 @@ import helper.packet.PlayerPositionMessage;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static helper.Constants.LOBBY_SIZE;
 import static helper.Constants.PORTS;
 
 public class GameServer {
     private final Server server;
+    private final List<Lobby> lobbies;
 
     public GameServer() {
         // setup server and open ports
         this.server = new Server();
+
+        // create lobbies
+        this.lobbies = new ArrayList<>();
+        // initialize default lobbies
+        for (int i = 0; i < 3; i++) {
+            Lobby lobby = new Lobby("Default lobby " + (i + 1), LOBBY_SIZE);
+            lobbies.add(lobby);
+        }
 
         // register used classes
         registerClasses();
@@ -36,26 +46,12 @@ public class GameServer {
 
         // add listener for new connections
         server.addListener(new Listener() {
-            Connection[] connections = new Connection[LOBBY_SIZE];
-
             @Override
             public void connected(Connection connection) {
                 super.connected(connection);
-                // loop through connections and add new connection to first empty slot
-                for (int i = 0; i < connections.length; i++) {
-                    // if connection is empty or not connected, add new connection
-                    if (connections[i] == null || !connections[i].isConnected()) {
-                        connections[i] = connection;
-                        break;
-                    }
-                }
-                if (Arrays.stream(connections).allMatch(c -> c != null && c.isConnected())) {
-                    // if all connections are filled, start game with the connections
-                    Game game = new Game(connections);
-                    game.start();
-                    // clear connections if all are connected and game is started
-                    connections = new Connection[LOBBY_SIZE];
-                }
+                lobbies.get(0).addConnection(connection);
+                // try to start game
+                lobbies.get(0).startGame();
             }
         });
     }
---
+++ server/src/main/java/ee/taltech/americandream/server/Lobby.java
@@ -0,0 +1,50 @@
+package ee.taltech.americandream.server;
+
+import com.esotericsoftware.kryonet.Connection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lobby {
+    private final int lobbySize;
+    private final String name;
+    private static int id = 1;
+    private final int lobbyId;
+    private final List<Connection> connections;
+
+    public Lobby(String name, int lobbySize) {
+        this.name = name;
+        this.lobbySize = lobbySize;
+        this.lobbyId = id;
+        this.connections = new ArrayList<>();
+
+        // increment id
+        id++;
+    }
+
+    private void removeDisconnected() {
+        connections.removeIf(connection -> !connection.isConnected());
+    }
+
+    public void addConnection(Connection connection) {
+        removeDisconnected();
+
+        // check if lobby is full
+        if (connections.size() >= lobbySize) return;
+
+        // check if connection is already in the lobby
+        if (connections.contains(connection)) return;
+
+        // add connection to the lobby
+        connections.add(connection);
+    }
+
+    public void startGame() {
+        // check if lobby is full
+        if (connections.size() < lobbySize) return;
+
+        // start game
+        Game game = new Game(connections.toArray(new Connection[0]));
+        game.start();
+    }
+}
