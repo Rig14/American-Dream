7575560496257ab2a48e913ad3b06b338e1f780c
ririvi <ririvi@taltech.ee>
2024-04-08 20:40:22 +0300

    AI: player now follows the closest player

--- server/src/main/java/ee/taltech/americandream/server/AIPlayer.java
+++ server/src/main/java/ee/taltech/americandream/server/AIPlayer.java
@@ -9,6 +9,7 @@ public class AIPlayer {
     private final List<BulletData> bullets;
     private float x;
     private float y;
+    private float velocity = 100;
 
     public AIPlayer(float x, float y) {
         this.x = x;
@@ -29,5 +30,30 @@ public class AIPlayer {
     }
 
     public void update(float delta, Player[] players) {
+        // select player closest to
+        Player closestPlayer = null;
+        float closestDistance = Float.MAX_VALUE;
+        for (Player player : players) {
+            float distance = (float) Math.sqrt(Math.pow(player.getState().x - x, 2) + Math.pow(player.getState().y - y, 2));
+            if (distance < closestDistance) {
+                closestDistance = distance;
+                closestPlayer = player;
+            }
+        }
+
+        if (closestPlayer == null) {
+            return;
+        }
+
+        // set velocity according to:
+        // when distance is higher -> velocity higher
+        // when distance is lower -> velocity lower
+        velocity = closestDistance;
+
+        // move towards player
+        float angle = (float) Math.atan2(closestPlayer.getState().y - y, closestPlayer.getState().x - x);
+
+        x += (float) (Math.cos(angle) * velocity * delta);
+        y += (float) (Math.sin(angle) * velocity * delta);
     }
 }
