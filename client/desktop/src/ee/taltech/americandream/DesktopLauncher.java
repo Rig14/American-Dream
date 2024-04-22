fdb36adcc1601befadedda33a81c3376e9bc1407
ririvi <ririvi@taltech.ee>
2024-04-22 16:49:17 +0300

    project: client now always starts in full screen

--- client/desktop/src/ee/taltech/americandream/DesktopLauncher.java
+++ client/desktop/src/ee/taltech/americandream/DesktopLauncher.java
@@ -8,6 +8,7 @@ public class DesktopLauncher {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setForegroundFPS(60);
+        config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
         config.setTitle("American Dream");
         new Lwjgl3Application(new AmericanDream(), config);
     }
